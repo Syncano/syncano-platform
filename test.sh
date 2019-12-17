f2bc2b1cf52011e3fa2d1df628615db241853ae6
Robert Kopaczewski <rk@23doors.com>
2019-12-17 21:24:34 +0100

    split response_templates sigh

--- test.sh
+++ test.sh
@@ -1,34 +1,37 @@
 #!/bin/bash
 set -e
 
-usage() { echo "* Usage: $0 [app_or_apps_to_test][--fast][--with-migrations][--skip-linter][--skip-coverage]" >&2; exit 1; }
+usage() {
+    echo "* Usage: $0 [app_or_apps_to_test][--fast][--with-migrations][--skip-linter][--skip-coverage]" >&2
+    exit 1
+}
 
 LINTER=true
 COVERAGE=true
 POSITIONAL=()
 
 # Parse arguments.
-for PARAM in $@; do
+for PARAM in "$@"; do
     case $PARAM in
-        --fast)
-          LINTER=false
-          COVERAGE=false
+    --fast)
+        LINTER=false
+        COVERAGE=false
         ;;
-        --with-migrations)
-          export TEST_MIGRATIONS=true
-          ;;
-        --skip-linter)
-          LINTER=false
-          ;;
-        --skip-coverage)
-          COVERAGE=false
-          ;;
-        --help)
-          usage
-          ;;
-        *)
-          POSITIONAL+=($PARAM)
-          ;;
+    --with-migrations)
+        export TEST_MIGRATIONS=true
+        ;;
+    --skip-linter)
+        LINTER=false
+        ;;
+    --skip-coverage)
+        COVERAGE=false
+        ;;
+    --help)
+        usage
+        ;;
+    *)
+        POSITIONAL+=("$PARAM")
+        ;;
     esac
 done
 
@@ -39,19 +42,28 @@ if $LINTER; then
 fi
 
 CMD="manage.py test --noinput --parallel ${PARALLEL_COUNT:-2} $*"
-
 if [ "${LEGACY_CODEBOX_ENABLED:-false}" != "true" ]; then
     CMD="${CMD} --exclude-tag legacy_codebox"
+fi
+
+if [ "$#" == 0 ]; then
+    CMDS=("${CMD} -e response_templates" "${CMD} response_templates")
+else
+    CMDS=("${CMD}")
 fi
 
 # Run tests
 export DJANGO_SETTINGS_MODULE=settings.tests
 if $COVERAGE; then
     coverage erase
-    coverage run $CMD
+    for cmd in "${CMDS[@]}"; do
+        coverage run "$cmd"
+    done
     coverage combine
 else
-    python $CMD
+    for cmd in "${CMDS[@]}"; do
+        python "$cmd"
+    done
 fi
 
 if $COVERAGE; then
