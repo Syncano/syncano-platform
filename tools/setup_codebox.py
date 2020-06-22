2c11534dee1dcf525852551912d59346078168c9
Robert Kopaczewski <rk@23doors.com>
2020-06-22 13:17:17 +0200

    setup

--- tools/setup_codebox.py
+++ tools/setup_codebox.py
@@ -1,27 +1,34 @@
 import django  # isort:skip
 import logging
-
+import os
 import docker
-from django.conf import settings
-
-from apps.core.helpers import docker_client
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.development")  # noqa
 django.setup()  # noqa
 
+from django.conf import settings
+from apps.core.helpers import docker_client
 
-logger = logging.getLogger(__name__)
+
+logger = logging.getLogger('django')
 
 try:
     docker_client.api.inspect_network(settings.DOCKER_NETWORK)
+    logger.info('Network "%s" already exists.', settings.DOCKER_NETWORK)
 except Exception:
+    logger.info('Creating network "%s".', settings.DOCKER_NETWORK)
+
     ipam_pool_config = docker.types.IPAMPool(settings.DOCKER_NETWORK_SUBNET)
     ipam_config = docker.types.IPAMConfig(pool_configs=[ipam_pool_config])
     docker_client.api.create_network(settings.DOCKER_NETWORK,
                                      check_duplicate=True,
                                      ipam=ipam_config)
 
-for container in docker_client.api.containers(filters={'label': 'workerId'}):
+containers = docker_client.api.containers(filters={'label': 'workerId'})
+
+logger.info('Removing old containers: %d.', len(containers))
+
+for container in containers:
     try:
         docker_client.api.remove_container(container['Id'], v=True, force=True)
     except docker.errors.APIError:
