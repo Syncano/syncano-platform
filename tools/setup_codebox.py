1083474d89970804c2835a0744f27098f2bb821d
Robert Kopaczewski <rk@23doors.com>
2020-06-23 15:36:37 +0200

    setup fixes

--- tools/setup_codebox.py
+++ tools/setup_codebox.py
@@ -9,6 +9,7 @@ import logging
 import docker
 from django.conf import settings
 
+from apps.codeboxes.runtimes import IMAGE
 from apps.core.helpers import docker_client
 
 logger = logging.getLogger(__name__)
@@ -34,3 +35,7 @@ for container in containers:
         docker_client.api.remove_container(container['Id'], v=True, force=True)
     except docker.errors.APIError:
         logger.warning("Docker container %s couldn't be removed.", container['Id'])
+
+
+print('Pulling container image: %s.' % IMAGE)
+docker_client.images.pull(IMAGE)
